2032840efd35f3757b43ba0663011a6ae31050ad
RFI21 <muh.rifai.ay21@gmail.com>
2026-06-18 14:54:13 +0800

    update

--- resources/views/user/administrasi.blade.php
+++ resources/views/user/administrasi.blade.php
@@ -534,6 +534,29 @@ var icons = {
         popupAnchor: [0, -35]
     }),
 
+    "Infrastruktur": L.divIcon({
+    html: `
+        <div style="
+            background:#64748b;
+            width:40px;
+            height:40px;
+            border-radius:50%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            color:white;
+            font-size:18px;
+            box-shadow:0 4px 10px rgba(0,0,0,0.3);
+        ">
+            <i class="bi bi-building-gear"></i>
+        </div>
+    `,
+    className: '',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -35]
+}),
+
 
        "UMKM": L.divIcon({
         html: `
